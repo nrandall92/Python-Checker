f525097a3fdb39b41cef4ea5cf0e08f02f4149ea
nrandall92 <nrandall@mail.umw.edu>
2014-12-02 12:56:17 -0500

    Update pythontables.sql

--- PythonChecker/pythontables.sql
+++ PythonChecker/pythontables.sql
@@ -2,7 +2,11 @@
 --
 --
 
-DROP DATABASE IF EXISTS Python_Checker;
+DROP TABLE Login;
+DROP TABLE Output;
+DROP TABLE Input;
+DROP TABLE Projects;
+DROP IF EXISTS Python_Checker;
 CREATE DATABASE Python_Checker;
 USE Python_Checker;
 
@@ -32,16 +36,7 @@ CREATE TABLE Output
 (
 	O_ID int(20) NOT NULL AUTO_INCREMENT,
 	Project_Name varchar(25) NOT NULL ,
-	/*Output_1 varchar(45),
-	Output_2 varchar(45),
-	Output_3 varchar(45),
-	Output_4 varchar(45),
-	Output_5 varchar(45),
-	Output_6 varchar(45),
-	Output_7 varchar(45),
-	Output_8 varchar(45),
-	Output_9 varchar(45),
-	Output_10 varchar(45),*/
+	Output_1 varchar(45),
 	PRIMARY KEY (O_ID)
 );
 
@@ -52,13 +47,8 @@ CREATE TABLE Input
 	PRIMARY KEY (O_ID)
 );
 
-/*CREATE TABLE Output 
-(
-	O_ID int(20) NOT NULL AUTO_INCREMENT,
-	Project_Name varchar(25) NOT NULL ,
-	PRIMARY KEY (O_ID)
-);*/
+
 
 INSERT INTO Login VALUES (1, 'Gusty', 'gus');
 INSERT INTO Projects VALUES (1, 'Sample');
-/*INSERT INTO Output VALUES (1, 'Sample', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10');*/
+
