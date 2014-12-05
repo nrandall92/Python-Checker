abe7fc49f25e09e8fa9555b28a20d0ca2b7ede33
nrandall92 <nrandall@mail.umw.edu>
2014-12-05 11:37:13 -0500

    Update pythontables.sql

--- PythonChecker/pythontables.sql
+++ PythonChecker/pythontables.sql
@@ -8,7 +8,7 @@ DROP TABLE Input;
 DROP TABLE Projects;
 DROP IF EXISTS Python_Checker;
 CREATE DATABASE Python_Checker;
-GRANT ALL PRIVILEGES ON Python_Checker to 'root'@'localhost' WITH GRANT OPTION IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON Python_Checker to 'root'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON Python_Checker to 'root'@'localhost' IDENTIFIED BY 'password';
 USE Python_Checker;
 
